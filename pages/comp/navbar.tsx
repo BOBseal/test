dd46083961e8d2b8bb0efd05f1b507d35c166ce3
Bishal Kr Bhuyan <94212983+BOBseal@users.noreply.github.com>
2022-12-05 01:33:29 +0530

    commit-v2

---
+++ pages/comp/navbar.tsx
@@ -0,0 +1,32 @@
+import Image from 'next/image'
+import Link from 'next/link'
+import React, { useState, useEffect } from 'react'
+import { AiOutlineClose, AiOutlineMail, AiOutlineMenu } from 'react-icons/ai'
+import { FaGithub, FaLinkedinIn } from 'react-icons/fa'
+import { BsFillPersonLinesFill } from 'react-icons/bs'
+import NavLogo from '../public/assets/navLogo.png'
+
+const Navbar = () => {
+    const [nav, setNav] = useState(false);
+    const [shadow, setShadow] = useState(false);
+    const [navBg, setNavBg] = useState('#ecf0f3');
+    const [linkColor, setLinkColor] = useState('#1f2937'); 
+    const handleNav = () => {
+      setNav(!nav);
+    };
+  
+    useEffect(() => {
+      const handleShadow = () => {
+        if (window.scrollY >= 90) {
+          setShadow(true);
+        } else {
+          setShadow(false);
+        }
+      };
+      window.addEventListener('scroll', handleShadow);
+    }, []);
+    return(
+<div>NNNNNNNNNNNNNNNNNNNNNNNNNNAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAVVVVVVVVVVVVVVVVVVVVVVVVVVVVVV</div>
+    )
+};
+export default Navbar
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,21 +1,33 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
-import TopNavbarLayout from '../layouts/TopNavbarLayout'
+import Navbar from './comp/navbar'
+
 const style = {
   wrapper: ` text-center align-center`,
   bluetext:`text-6xl pt-6 text-[#000099] transitions-all hover:text-[#4d4dff] font-style: italic font-bold `,
-  footText:`text-2xl pl-40 text-left font-semibold text-gray-600 `,
+  footText:`text-2xl pl-40 text-left font-regular text-gray-600 `,
   underline:`hover:underline`
 }
 /*console.log(NavMenus)*/
 
 export default function Home() {
   return (
- <div className={style.wrapper}> <div className={style.underline}>
+ <div className={style.wrapper}>
+      <Head>
+        <title>BOBSEAL | Bishal Bhuyan</title>
+        <meta name="description" content="Chuckes! I am a Danger" />
+        <link rel="icon" href="/logo.ico" />
+      </Head>
+   
+    
+     <div className='bg-cyan-300 h-auto m-auto shadow-xl shadow-cyan-300 rounded-xl flex items-center justify-center p-4 hover:scale-105 ease-in duration-300'>  <Navbar/></div>
+     <div className={style.underline}><div className=' hover:scale-105 ease-in duration-200'>
    <div className={style.bluetext}>Welcome to BOB's Site
-   </div></div>
-   <footer className='pt-8'> <div className={style.footText}>
+   </div></div> </div>
+   <button className=' h-auto m-auto shadow-xl shadow-gray-400 rounded-xl flex items-center justify-center p-3 hover:scale-105 ease-in duration-300 font-bold'> Click</button>
+   <footer className='pt-8'> 
+   <div className={style.footText}>
      This is just testing</div>
    </footer>
  </div>
